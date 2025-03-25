71d6c478427e678b86a9379ffb13efebb7c36fb5
a <a>
2025-03-25 07:58:57 +0100

    v2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,31 +1,31 @@
 let kolko = [
-    {start: "0", end: "20", color: "#baffc9"},
-    {start: "30", end: "60", color: "#ffffba"},
-    {start: "60", end: "85", color: "#ffdfba"},
-    {start: "85", end: "100", color: "#bae1ff"},
+    { start: "0", end: "20", color: "#baffc9" },
+    { start: "30", end: "60", color: "#ffffba" },
+    { start: "60", end: "85", color: "#ffdfba" },
+    { start: "85", end: "100", color: "#bae1ff" },
 ];
 
 let sprzedazpizzy = [
-    {ilosc: "30"},
-    {ilosc: "50"},
-    {ilosc: "80"},
-    {ilosc: "90"},
-    {ilosc: "35"}
+    { ilosc: "30" },
+    { ilosc: "50" },
+    { ilosc: "80" },
+    { ilosc: "90" },
+    { ilosc: "35" }
 ];
 let cena = [
-    {oldPrice: "20.98", newPrice: "34.87" }
+    { oldPrice: "20.98", newPrice: "34.87" }
 ];
 let clients = [
-    {month1: "746", month2: "938"}
+    { month1: "746", month2: "938" }
 ];
 let skladniki = [
-{nazwa: "szynka", procent1: "50",procent2 : "10"},
-{nazwa: "Pieczarki", procent1: "80",procent2 : "30"},
-{nazwa: "Papryka", procent1: "90",procent2 : "65"},
-{nazwa: "Cebula", procent1: "20",procent2 : "75"},
-{nazwa: "Oliwki", procent1: "25",procent2 : "40"},
+    { nazwa: "szynka", procent1: "50", procent2: "10" },
+    { nazwa: "Pieczarki", procent1: "80", procent2: "30" },
+    { nazwa: "Papryka", procent1: "90", procent2: "65" },
+    { nazwa: "Cebula", procent1: "20", procent2: "75" },
+    { nazwa: "Oliwki", procent1: "25", procent2: "40" },
 ];
-    
+
 
 let kolo = document.querySelector("#kolo");
 let kol1 = document.querySelector(".one");
@@ -38,7 +38,7 @@ let kol5 = document.querySelector(".five");
 let NowiKlienci = document.getElementById("NowiKlienci");
 let przyrost2 = document.getElementById("przyrost2");
 let roznica2 = clients[0].month2 - clients[0].month1;
-let obliczenie2 = (roznica2 / clients[0].month1)*100;
+let obliczenie2 = (roznica2 / clients[0].month1) * 100;
 NowiKlienci.innerText = `${clients[0].month2}`;
 przyrost2.innerText = `${obliczenie2.toFixed(2)}%`
 
@@ -48,7 +48,7 @@ polygon.style.clipPath = `polygon(${skladniki[0].procent1}% ${skladniki[0].proce
 let NowaCena = document.getElementById("NowaCena");
 let przyrost = document.getElementById("przyrost");
 let roznica = cena[0].newPrice - cena[0].oldPrice;
-let obliczenie = (roznica / cena[0].oldPrice )*100;
+let obliczenie = (roznica / cena[0].oldPrice) * 100;
 NowaCena.innerText = `${cena[0].newPrice}$`;
 przyrost.innerText = `${obliczenie.toFixed(2)}%`;
 console.log(obliczenie.toFixed(2) + "%");
@@ -69,35 +69,38 @@ let gradient = kolko.map(segment => {
 kolo.style.background = `conic-gradient(${gradient})`;
 
 let draggedBox = null;
- 
-        const boxes = document.querySelectorAll('.divGrid');
- 
-        boxes.forEach(box => {
-            box.addEventListener('mousedown', (e) => {
-                draggedBox = box;
-                box.classList.add('highlight');
-            });
- 
-            box.addEventListener('mouseup', () => {
-                if (draggedBox) {
-                    draggedBox.classList.remove('highlight'); 
-                    draggedBox = null;
-                }
-            });
- 
-            box.addEventListener('mouseenter', () => {
-                if (draggedBox && draggedBox !== box) {
-                
-                    const tempContent = box.innerHTML;
-                    box.innerHTML = draggedBox.innerHTML;
-                    draggedBox.innerHTML = tempContent;
- 
-                    draggedBox.classList.remove('highlight');
-                    draggedBox = null;
-                }
-            });
-        });
- 
-        document.addEventListener('dragstart', (e) => {
-            e.preventDefault();
-        });
+
+const boxes = document.querySelectorAll('.divGrid');
+
+boxes.forEach(box => {
+    box.addEventListener('mousedown', (e) => {
+        draggedBox = box;
+        box.classList.add('highlight');
+    });
+
+    box.addEventListener('mouseup', () => {
+        if (draggedBox) {
+            draggedBox.classList.remove('highlight');
+            draggedBox = null;
+        }
+    });
+
+    box.addEventListener('mouseenter', () => {
+        if (draggedBox && draggedBox !== box) {
+
+            const tempContent = box.innerHTML;
+            box.innerHTML = draggedBox.innerHTML;
+            draggedBox.innerHTML = tempContent;
+
+            draggedBox.classList.remove('highlight');
+            draggedBox = null;
+
+            draggedBox = box;
+            box.classList.add('highlight');
+        }   
+    });
+});
+
+document.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+});
